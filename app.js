33011f4b70227e2bb63d6fae9b3f1adc0050915d
unknown <okpusjamin@gmail.com>
2018-11-28 11:42:26 +0100

    add remote DB

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app = express();
 
 // MONGOOSE CONFIG
 app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost/saveIT");
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds119374.mlab.com:19374/saveit");
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
